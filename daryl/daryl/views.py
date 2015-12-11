d64432ff1a5d44fa8c1f239f08ae1f24f66e5659
johanna <johanna.lee@magnetforensics.com>
2015-12-10 16:42:43 -0800

    more broken codes

--- daryl/daryl/views.py
+++ daryl/daryl/views.py
@@ -5,31 +5,30 @@ from django.template import RequestContext, loader
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
-from django.shortcuts import get_list_or_404
-
-class Image:
-	def __init__(self, filename, category):
-		self.filename = filename
-		self.category = category
-
-	def from_json(self):
-		data = json.loads(open('../sample.json').read())['images']
-		cnt = 1;
-		for cat in data['categories']:
-			name = cat['name']
-			Category.objects.create(name=name,category_id=cnt)
-			Category.save()
-			cnt += 1
-
-		for img in data['images']:
-			filename = img['filename']
-			category = img['category']
-			Image.objects.create(filename=filename,category=category)
-			Image.save()
+from daryl.models import *
 
 def test(request):
     template = loader.get_template('test.html')
     return HttpResponse(template.render())
 
 def index(request):
-	list_image = get_list_or_404(Image)
+	data = json.loads(open('../sample.json').read())['images']
+	cnt = 1;
+	for cat in data['categories']:
+		name = cat['name']
+		Category.objects.create(name=name,category_id=cnt)
+		Category.save()
+		cnt += 1
+	for img in data['images']:
+		filename = img['filename']
+		category = img['category']
+		Image.objects.create(filename=filename,category=category)
+		Image.save()
+	image_list = Image.objects.all()
+	category_list = Category.objects.all()
+	template = loader.get_template('index.html')
+	context = RequestContext(request, {
+		'images': image_list,
+		'categories': category_list,
+		})
+	return HttpResponse(template.render(context))
